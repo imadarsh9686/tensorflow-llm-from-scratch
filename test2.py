8b93d9ede0236583f2beaf3884ebb1fa01c4af2b
Adarsh I Metimath <113964790+imadarsh9686@users.noreply.github.com>
2023-06-24 02:18:28 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -5,6 +5,9 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 assert tf.__version__.startswith('2')
 tf.random.set_seed(1234)
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
 
 import os
 
