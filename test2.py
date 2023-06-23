dd0bc5681b38aabfc1ca64a1d677677a4418a3c0
Adarsh I Metimath <113964790+imadarsh9686@users.noreply.github.com>
2023-06-24 03:19:51 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -5,8 +5,8 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 assert tf.__version__.startswith('2')
 tf.random.set_seed(1234)
+
 import os
-#os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 import numpy as np
 
@@ -36,9 +36,6 @@ path_to_movie_conversations = os.path.join('movie_conversations.txt')
 
 # Maximum number of samples to preprocess
 MAX_SAMPLES = 50000
-from tensorflow import keras
-
-
 
 
 
@@ -108,65 +105,123 @@ START_TOKEN, END_TOKEN = [tokenizer.vocab_size], [tokenizer.vocab_size + 1]
 # Vocabulary size plus start and end token
 VOCAB_SIZE = tokenizer.vocab_size + 2
 
-@keras.utils.register_keras_serializable()
-class MultiHeadAttention(tf.keras.layers.Layer):
-    def __init__(self, d_model, num_heads, name="multi_head_attention"):
-        super(MultiHeadAttention, self).__init__(name=name)
-        self.num_heads = num_heads
-        self.d_model = d_model
-    
-        assert d_model % self.num_heads == 0
-    
-        self.depth = d_model // self.num_heads
-    
-        self.query_dense = tf.keras.layers.Dense(units=d_model)
-        self.key_dense = tf.keras.layers.Dense(units=d_model)
-        self.value_dense = tf.keras.layers.Dense(units=d_model)
-    
-        self.dense = tf.keras.layers.Dense(units=d_model)
 
-    
+# Tokenize, filter and pad sentences
+def tokenize_and_filter(inputs, outputs):
+  tokenized_inputs, tokenized_outputs = [], []
+
+  for (sentence1, sentence2) in zip(inputs, outputs):
+    # tokenize sentence
+    sentence1 = START_TOKEN + tokenizer.encode(sentence1) + END_TOKEN
+    sentence2 = START_TOKEN + tokenizer.encode(sentence2) + END_TOKEN
+    # check tokenized sentence max length
+    if len(sentence1) <= MAX_LENGTH and len(sentence2) <= MAX_LENGTH:
+      tokenized_inputs.append(sentence1)
+      tokenized_outputs.append(sentence2)
+
+  # pad tokenized sentences
+  tokenized_inputs = tf.keras.preprocessing.sequence.pad_sequences(
+      tokenized_inputs, maxlen=MAX_LENGTH, padding='post')
+  tokenized_outputs = tf.keras.preprocessing.sequence.pad_sequences(
+      tokenized_outputs, maxlen=MAX_LENGTH, padding='post')
+
+  return tokenized_inputs, tokenized_outputs
+
+
+questions, answers = tokenize_and_filter(questions, answers)
+
+
+BATCH_SIZE = 64
+BUFFER_SIZE = 20000
+
+# decoder inputs use the previous target as input
+# remove START_TOKEN from targets
+dataset = tf.data.Dataset.from_tensor_slices((
+    {
+        'inputs': questions,
+        'dec_inputs': answers[:, :-1]
+    },
+    {
+        'outputs': answers[:, 1:]
+    },
+))
+
+dataset = dataset.cache()
+dataset = dataset.shuffle(BUFFER_SIZE)
+dataset = dataset.batch(BATCH_SIZE)
+dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
+
+
+def scaled_dot_product_attention(query, key, value, mask):
+  """Calculate the attention weights. """
+  matmul_qk = tf.matmul(query, key, transpose_b=True)
+
+  # scale matmul_qk
+  depth = tf.cast(tf.shape(key)[-1], tf.float32)
+  logits = matmul_qk / tf.math.sqrt(depth)
+
+  # add the mask to zero out padding tokens
+  if mask is not None:
+    logits += (mask * -1e9)
+
+  # softmax is normalized on the last axis (seq_len_k)
+  attention_weights = tf.nn.softmax(logits, axis=-1)
+
+  output = tf.matmul(attention_weights, value)
+
+  return output
+
+
+class MultiHeadAttention(tf.keras.layers.Layer):
+
+  def __init__(self, d_model, num_heads, name="multi_head_attention"):
+    super(MultiHeadAttention, self).__init__(name=name)
+    self.num_heads = num_heads
+    self.d_model = d_model
+
+    assert d_model % self.num_heads == 0
+
+    self.depth = d_model // self.num_heads
+
+    self.query_dense = tf.keras.layers.Dense(units=d_model)
+    self.key_dense = tf.keras.layers.Dense(units=d_model)
+    self.value_dense = tf.keras.layers.Dense(units=d_model)
+
+    self.dense = tf.keras.layers.Dense(units=d_model)
+
   def split_heads(self, inputs, batch_size):
-      
-      inputs = tf.reshape(inputs, shape=(batch_size, -1, self.num_heads, self.depth))
-      return tf.transpose(inputs, perm=[0, 2, 1, 3])
-      
-    
+    inputs = tf.reshape(
+        inputs, shape=(batch_size, -1, self.num_heads, self.depth))
+    return tf.transpose(inputs, perm=[0, 2, 1, 3])
 
   def call(self, inputs):
-      
-      query, key, value, mask = inputs['query'], inputs['key'], inputs['value'], inputs['mask']
-      batch_size = tf.shape(query)[0]
-    
-      # linear layers
-      query = self.query_dense(query)
-      key = self.key_dense(key)
-      value = self.value_dense(value)
-    
-      # split heads
-      query = self.split_heads(query, batch_size)
-      key = self.split_heads(key, batch_size)
-      value = self.split_heads(value, batch_size)
-    
-      # scaled dot-product attention
-      scaled_attention = scaled_dot_product_attention(query, key, value, mask)
-    
-      scaled_attention = tf.transpose(scaled_attention, perm=[0, 2, 1, 3])
-    
-      # concatenation of heads
-      concat_attention = tf.reshape(scaled_attention,
-                                  (batch_size, -1, self.d_model))
-    
-      # final linear layer
-      outputs = self.dense(concat_attention)
-    
-      return outputs
-      
-    
-    
-    
-    # Your custom implementation
+    query, key, value, mask = inputs['query'], inputs['key'], inputs[
+        'value'], inputs['mask']
+    batch_size = tf.shape(query)[0]
 
+    # linear layers
+    query = self.query_dense(query)
+    key = self.key_dense(key)
+    value = self.value_dense(value)
+
+    # split heads
+    query = self.split_heads(query, batch_size)
+    key = self.split_heads(key, batch_size)
+    value = self.split_heads(value, batch_size)
+
+    # scaled dot-product attention
+    scaled_attention = scaled_dot_product_attention(query, key, value, mask)
+
+    scaled_attention = tf.transpose(scaled_attention, perm=[0, 2, 1, 3])
+
+    # concatenation of heads
+    concat_attention = tf.reshape(scaled_attention,
+                                  (batch_size, -1, self.d_model))
+
+    # final linear layer
+    outputs = self.dense(concat_attention)
+
+    return outputs
 
 
 
