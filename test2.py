41fc419adc92c9299782ee299b4714286fe8d191
Adarsh I Metimath <113964790+imadarsh9686@users.noreply.github.com>
2023-06-24 12:07:52 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -130,9 +130,76 @@ dataset = dataset.batch(BATCH_SIZE)
 dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
 
 
+def scaled_dot_product_attention(query, key, value, mask):
+  """Calculate the attention weights. """
+  matmul_qk = tf.matmul(query, key, transpose_b=True)
+
+  # scale matmul_qk
+  depth = tf.cast(tf.shape(key)[-1], tf.float32)
+  logits = matmul_qk / tf.math.sqrt(depth)
+
+  # add the mask to zero out padding tokens
+  if mask is not None:
+    logits += (mask * -1e9)
+
+  # softmax is normalized on the last axis (seq_len_k)
+  attention_weights = tf.nn.softmax(logits, axis=-1)
+
+  output = tf.matmul(attention_weights, value)
+
+  return output
 
 
+class MultiHeadAttention(tf.keras.layers.Layer):
 
+  def __init__(self, d_model, num_heads, name="multi_head_attention"):
+    super(MultiHeadAttention, self).__init__(name=name)
+    self.num_heads = num_heads
+    self.d_model = d_model
+
+    assert d_model % self.num_heads == 0
+
+    self.depth = d_model // self.num_heads
+
+    self.query_dense = tf.keras.layers.Dense(units=d_model)
+    self.key_dense = tf.keras.layers.Dense(units=d_model)
+    self.value_dense = tf.keras.layers.Dense(units=d_model)
+
+    self.dense = tf.keras.layers.Dense(units=d_model)
+
+  def split_heads(self, inputs, batch_size):
+    inputs = tf.reshape(
+        inputs, shape=(batch_size, -1, self.num_heads, self.depth))
+    return tf.transpose(inputs, perm=[0, 2, 1, 3])
+
+  def call(self, inputs):
+    query, key, value, mask = inputs['query'], inputs['key'], inputs[
+        'value'], inputs['mask']
+    batch_size = tf.shape(query)[0]
+
+    # linear layers
+    query = self.query_dense(query)
+    key = self.key_dense(key)
+    value = self.value_dense(value)
+
+    # split heads
+    query = self.split_heads(query, batch_size)
+    key = self.split_heads(key, batch_size)
+    value = self.split_heads(value, batch_size)
+
+    # scaled dot-product attention
+    scaled_attention = scaled_dot_product_attention(query, key, value, mask)
+
+    scaled_attention = tf.transpose(scaled_attention, perm=[0, 2, 1, 3])
+
+    # concatenation of heads
+    concat_attention = tf.reshape(scaled_attention,
+                                  (batch_size, -1, self.d_model))
+
+    # final linear layer
+    outputs = self.dense(concat_attention)
+
+    return outputs
       
 def create_padding_mask(x):
   mask = tf.cast(tf.math.equal(x, 0), tf.float32)
@@ -146,7 +213,11 @@ def create_look_ahead_mask(x):
   return tf.maximum(look_ahead_mask, padding_mask)
 
 
-model = tf.keras.models.load_model('DTLM_V4_model')
+with keras.utils.CustomObjectScope({'MultiHeadAttention': MultiHeadAttention}):
+    
+    model = tf.keras.models.load_model('DTLM_V4_model')
+
+#model = tf.keras.models.load_model('DTLM_V4_model')
 
 
 def evaluate(sentence):
