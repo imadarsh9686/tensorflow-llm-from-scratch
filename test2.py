dcced16eb6583c9120224357da079685cf5305c8
Adarsh I Metimath <113964790+imadarsh9686@users.noreply.github.com>
2023-07-04 16:32:07 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -3,7 +3,7 @@ import json
 import re
 import tensorflow as tf
 import tensorflow_datasets as tfds
-assert tf.__version__.startswith('2')
+#assert tf.__version__.startswith('2')
 tf.random.set_seed(1234)
 from tensorflow import keras
 
